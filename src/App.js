392bceb298be62b72b1cf90ec0025b7ee4941277
Devanshshah1309 <59191109+Devanshshah1309@users.noreply.github.com>
2021-12-30 13:27:10 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import Navbar from './components/navbar';
+import Counters from './components/counters';
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    counters: [
+        {id: "CS1101S", value: 0},
+        {id: "CS12131S", value: 0},
+        {id: "MA1521", value: 0},
+        {id: "MA2001", value: 0}
+
+    ]
+};
+handleDelete = (counter) => {
+    const newCounters = this.state.counters.filter(x => x.id!== counter.id);
+    this.setState({counters: newCounters});
+}
+handleReset = () => {
+    const newCounters = this.state.counters.map(x => {
+        x.value = 0;
+        return x;
+    });
+    this.setState({counters: newCounters});
+}
+handleIncrement = (counter) => {
+    const newCounters = [...this.state.counters];
+    const index = newCounters.indexOf(counter);
+    newCounters[index] = {...counter};
+    newCounters[index].value++;
+    this.setState({counters: newCounters});
+}
+
+handleDecrement = (counter) => {
+  const newCounters = [...this.state.counters];
+  const index = newCounters.indexOf(counter);
+  newCounters[index] = {...counter};
+  if (newCounters[index].value > 0) {
+    newCounters[index].value--;
+  }
+  this.setState({counters: newCounters});
+}
+render() {
+  return (
+    <React.Fragment>
+      <Navbar counters = {this.state.counters}/>
+      <main className='container'>
+        <Counters 
+          onReset = {this.handleReset}
+          onIncrement = {this.handleIncrement}
+          onDecrement = {this.handleDecrement}
+          onDelete = {this.handleDelete}
+          counters = {this.state.counters} />
+      </main> 
+    </React.Fragment>
+  
+  );
+}
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+//Stateless functional component (we don't need render())
+//You can use sfc to create template for a Stateless functional Component
+
+//use {} for obejct destructuring (so you don't have to keep sayig props.whatver)
+const Navbar = ({counters}) => {
+    let totalTasks = 0;
+    for (let index = 0; index < counters.length; index++) {
+        totalTasks += counters[index].value;     
+    }
+    return  (<nav className="navbar navbar-light bg-light">
+        <a className ="navbar-brand" href="#">
+            <span className="badge badge-pill badge-secondary m-2">
+                Total tasks pending: {totalTasks}
+            </span>
+        </a>
+      </nav>
+    );
+}
+
+ 
+export default Navbar;
